6b57a0885073f103019c41e7702a904ffd1d428f
JojoSnow <milzwow94@gmail.com>
2022-01-24 11:52:39 +0200

    heroku push osa3c

--- index.js
+++ index.js
@@ -1,45 +1,35 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
+const Person = require('./models/person');
 
 app.use(express.json());
-app.use(cors())
-app.use(express.static('build'))
+app.use(cors());
+app.use(express.static('build'));
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    }
-]
-
-app.get('/', (request, response) => {
-  response.send('<h1>Persons</h1>')
-})
+if (process.argv.length === 3) {
+    console.log(`phonebook:`) 
+    Person.find({}).then(result => {
+        result.forEach(p => {
+            console.log(p.name, p.number);
+        });
+    });
+} else if (process.argv.length > 3) {
+    person.save().then(result => {
+        console.log(`added ${result.name} number ${result.number} to phonebook`);
+    });
+}
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(ps => {
+      response.json(ps)
+  })
 })
 
 app.get('/info', (request, response) => {
@@ -49,32 +39,37 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const person = persons.find(p => p.id === id);
+    Person.findById(request.params.id).then(p => {
+        if (!p) {
+            return response.status(400).json({
+                error: 'content missing'
+            });
+        } else {
+            return response.json(p);
+        }
+    });
 
-    if (!person) {
-        return response.status(400).json({
-            error: 'content missing'
-        });
-    } else {
-        return response.json(person);
-    }
+    
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    persons = persons.filter(person => person.id !== id);
+// app.delete('/api/persons/:id', (request, response) => {
+//     const id = Number(request.params.id);
+//     persons = persons.filter(person => person.id !== id);
 
-    response.status(204).end();
-})
-
-const generateId = (min, max) => {
-    return Math.floor(Math.random() * (max - min) + min);
-}
+//     response.status(204).end();
+// })
 
 app.post('/api/persons', (request, response) => {
     const body = request.body;
-    const name = persons.find(p => p.name === body.name)
+    
+    if (body.content === undefined) {
+        return response.status(400).json({ error: 'content missing'});
+    }
+
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    })
 
     if (!body.name) {
         return response.status(400).json({
@@ -84,24 +79,19 @@ app.post('/api/persons', (request, response) => {
         return response.status(400).json({
             error: 'number missing'
         });
-    } else if (name) {
+    } else if (body.name) {
         return response.status(400).json({
             error: 'name must be unique'
         });
     }
 
-    const person = {
-        id: generateId(5, 10000),
-        name: body.name,
-        number: body.number
-    }
-
-    persons = persons.concat(person);
-
-    response.json(body);
+    person.save().then(savedPerson => {
+        response.json(savedPerson);
+    });
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
