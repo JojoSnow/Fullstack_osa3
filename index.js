7216b63c9fa7f215ec55d7f99a2d7f1823f23e23
JojoSnow <milzwow94@gmail.com>
2022-01-25 09:21:07 +0200

    osa3c 3.15 step3 done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 const Person = require('./models/person');
+const { response } = require('express');
 
 app.use(express.json());
 app.use(cors());
@@ -14,16 +15,17 @@ morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 app.get('/api/persons', (request, response) => {
-  Person.find({}).then(ps => {
-      response.json(ps)
-  })
-})
+    Person.find({}).then(ps => {
+        response.json(ps)
+    });
+});
 
 app.get('/info', (request, response) => {
     const peopleAmount = persons.length;
     const reqTime = new Date();
+
     response.send(`<p>Phonebook has info for ${peopleAmount} people</p> <p>${reqTime}</p>`);
-})
+});
 
 app.get('/api/persons/:id', (request, response) => {
     Person.findById(request.params.id).then(p => {
@@ -35,18 +37,18 @@ app.get('/api/persons/:id', (request, response) => {
             return response.json(p);
         }
     }); 
-})
+});
 
-// app.delete('/api/persons/:id', (request, response) => {
-//     const id = Number(request.params.id);
-//     persons = persons.filter(person => person.id !== id);
-
-//     response.status(204).end();
-// })
+app.delete('/api/persons/:id', (request, response) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end();
+        })
+        .catch(error => next(error));
+});
 
 app.post('/api/persons', (request, response) => {
     const body = request.body;
-    console.log(body)
     
     const person = new Person({
         name: body.name,
@@ -56,7 +58,7 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson);
     });
-})
+});
 
 const PORT = process.env.PORT;
 
